16dd083ef79f7a780433b0c12ca966f2960fd835
Kevin Sorensen <kevin@fdtr.ca>
2019-02-21 23:24:33 -0700

    change app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            CHanged!
+            Feature Change
           </p>
           <a
             className="App-link"
